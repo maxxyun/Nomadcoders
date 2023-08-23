edbb6a0e261d24d69baf040cad0685a7ba184d37
maxxyun <windogs09@gmail.com>
2023-08-23 18:33:28 +0900

    0523

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
-# 2.3 Recap
-my_name = "nico"
-age = 12
-dead = False
-print("Hello my name is", my_name)
-print("and I'm", age, "years old")
+# # 2.3 Recap
+# my_name = "nico"
+# age = 12
+# dead = False
+# print("Hello my name is", my_name)
+# print("and I'm", age, "years old")
+#
+# # 2.4 Functions
+# print(True)
+# print("hello")
+# print(12)
+# print(True, "hello", 12)
+#
+#
+# def say_hello():
+#   print("hello how R U?")
+#
+# say_hello()
+# say_hello()
+# say_hello()
+# print("hello world")
 
-# 2.4 Functions
-print(True)
-print("hello")
-print(12)
-print(True, "hello", 12)
+# # 2.5 Indentation
+# def say_hello():
+#   print("hello how R U?")
+# def say_bye():
+#   print("bye bye")
+#
+# say_hello()
+# say_bye()
 
+#2.6 Parameters
+def say_hello(user_name):
+  print("hello", user_name, "how R U?")
 
-def say_hello():
-  print("hello how R U?")
-
-
-say_hello()
-say_hello()
-say_hello()
-print("hello world")
-
-# 2.5 Indentation
+say_hello("hongku")

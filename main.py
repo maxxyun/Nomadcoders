fd0cda2c5f4ce82d22d42a1d84c1e8de7ccd702f
maxxyun <windogs09@gmail.com>
2023-08-24 18:43:39 +0900

    0824

--- main.py
+++ main.py
@@ -50,9 +50,30 @@
 # tax_calculator(150)
 
 # 2.9 Default Parameters
-def say_hello(user_name="anonymous"):
-    print("hello", user_name)
+# def say_hello(user_name="anonymous"):
+#     print("hello", user_name)
+#
+# say_hello("nico")
+# say_hello()
 
 
-say_hello("nico")
-say_hello()
+# def plus(a=0, b=0):
+#     print(a + b)
+#
+# def minus(a=0, b=0):
+#     print(a - b)
+#
+# def multiple(a=0, b=0):
+#     print(a * b)
+#
+# def divide(a=0, b=1):
+#     print(a / b)
+#
+# def power(a=0, b=0):
+#     print(a ** b)
+#
+# plus()
+# minus()
+# multiple()
+# divide()
+# power()

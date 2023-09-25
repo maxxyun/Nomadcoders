212508192ec121d605431af6eb6a775f761cbadd
maxxyun <windogs09@gmail.com>
2023-09-25 18:25:33 +0900

    0925

--- main.py
+++ main.py
@@ -201,8 +201,27 @@ elif user_choice < pc_choice:
 
 # 3.5 While
 
-distance = 0
+# distance = 0
+#
+# while distance < 20:
+#     print("I'm running", distance, "km")
+#     distance = distance + 1
 
-while distance < 20:
-    print("I'm running", distance, "km")
-    distance = distance + 1
+# 3.6 Python Casino
+
+from random import randint
+
+print("Welcome to Python Casino")
+pc_choice = randint(1, 100)
+
+playing = True
+
+while playing:
+    user_choice = int(input("Choose number (1-100) :"))
+    if user_choice == pc_choice:
+        print("You won!")
+        playing = False
+    elif user_choice > pc_choice:
+        print("Lower!")
+    elif user_choice < pc_choice:
+        print("Higher!")

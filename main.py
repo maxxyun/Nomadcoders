f4dde47e432d5c7a0d304e962275c8c2eddc082e
maxxyun <windogs09@gmail.com>
2023-10-04 18:54:23 +0900

    1004

--- main.py
+++ main.py
@@ -262,7 +262,7 @@ elif user_choice < pc_choice:
 
 # 4.1 Lists
 
-days_of_week = ["Mon","Tue","Wed","Thur","Fri"]
+days_of_week = ["Mon","Tue","Wed","Thur","Fri", 1, 2, 3, True, False, "hi", "black", [1, 2, 3, [False, True]]]
 
 print(days_of_week)
 
@@ -274,3 +274,5 @@ print(days_of_week)
 
 print(days_of_week[1])
 print(days_of_week[2])
+
+# 4.2 Tuples

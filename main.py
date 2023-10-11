c9c5818144d1210c4431d272d6b643fd4df25390
maxxyun <windogs09@gmail.com>
2023-10-11 18:49:58 +0900

    1011

--- main.py
+++ main.py
@@ -283,28 +283,29 @@ elif user_choice < pc_choice:
 
 # 4.3 Dicts
 
- player= {
-     'name': 'nico',
-     'age': 12,
-     "alive": True
+player={
+     'name':'nico',
+     'age':12,
+     # 'alive':True
      'fav_food': ["🍕","🍔"]
  }
 
- print(player.get('age'))
- print(player.get('fav_food'))
- print(player['fav_food'])
-
- print(player)
- player.pop('age')  # age를 지움
- print(player)
-
- print(player)
- player['xp']=1500  # 추가
- print(player)
-
- print(player)
- player['fav_food'].append("☕")
- print(player.get('fav_food'))
- print(player['fav_food'])
+print(player)
+ # print(player.get('age'))
+ # print(player.get('fav_food'))
+ # print(player['fav_food'])
+ #
+ # print(player)
+ # player.pop('age')  # age를 지움
+ # print(player)
+ #
+ # print(player)
+ # player['xp']=1500  # 추가
+ # print(player)
+ #
+ # print(player)
+ # player['fav_food'].append("☕")
+ # print(player.get('fav_food'))
+ # print(player['fav_food'])
 
 # 4.3 한번 더 보기

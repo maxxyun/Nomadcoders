e3e9639936fd60ceff61345b6eb66f4c906fd199
maxxyun <windogs09@gmail.com>
2023-10-11 18:26:03 +0900

    1011

--- main.py
+++ main.py
@@ -277,8 +277,17 @@ elif user_choice < pc_choice:
 
 # 4.2 Tuples
 
-days = ("Mon", "Tue", "Wed")    # 튜플은 내부 데이터를 변경하지 못한다. .count랑 .index만 사용 가능.
-
-print(days[-2])  # 튜플은 괄호가 리스트랑 반대. 만들때는 ( ) 뭔가 할때는 [ ] 를 쓴다.
+# days = ("Mon", "Tue", "Wed")    # 튜플은 내부 데이터를 변경하지 못한다. .count랑 .index만 사용 가능.
+#
+# print(days[-2])  # 튜플은 괄호가 리스트랑 반대. 만들때는 ( ) 뭔가 할때는 [ ] 를 쓴다.
 
 # 4.3 Dicts
+
+ player= {
+     'name' : 'nico',
+     'age' : 12,
+     'alive' : True
+     'fav_food': [""]
+ }
+
+ print(player.get('age'))

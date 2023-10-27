5ad91a52fa83da7fc1680bf3fa8e6fdab7ed803f
maxxyun <windogs09@gmail.com>
2023-10-27 18:03:07 +0900

    1027

---
+++ 2.6Parameters.py
@@ -0,0 +1,6 @@
+# 2.6 Parameters
+def say_hello(user_name):
+    print("hello", user_name, "how R U?")
+
+
+say_hello("hongku")
---
+++ 2.7Multiple Parameters.py
@@ -0,0 +1,7 @@
+# 2.7 Multiple Parameters
+def say_hello(user_name, user_age):
+    print("hello", user_name)
+    print("you are", user_age, "years old")
+
+
+say_hello("hongku", 12)

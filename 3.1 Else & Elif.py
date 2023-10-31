8e035e5b20a082dd794f4291cf9bf1275483639f
maxxyun <windogs09@gmail.com>
2023-10-31 18:44:35 +0900

    1031

---
+++ 3.1 Else & Elif.py
@@ -0,0 +1,14 @@
+# 3.1 Else & Elif
+password_correct = False
+if password_correct:
+    print("Here is your money")
+else:
+    print("Wrong password")
+
+winner = 10
+if winner > 10:
+    print("Winner is greater than 10")
+elif winner <= 10:
+    print("Winner is less than 10")
+else:
+    print("Winner is 10")

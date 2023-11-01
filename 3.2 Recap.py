61a6b3af93beab7a499104c399d75162a57f1497
maxxyun <windogs09@gmail.com>
2023-11-01 18:52:40 +0900

    1101

---
+++ 3.2 Recap.py
@@ -0,0 +1,12 @@
+# 3.2 Recap
+winner = 50  # >, <, >=, <=, ==, !=
+if winner != 10:
+    print("If")
+elif winner <= 25:
+    print("elif")
+elif winner == 0:
+    print("elif 2")
+elif winner == 50:
+    print("elif 3")
+else:
+    print("Else")

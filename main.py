0adc183512f018aaf077e75b7fe3d8678ec7a51c
maxxyun <windogs09@gmail.com>
2023-11-03 18:51:33 +0900

    1103

--- main.py
+++ main.py
@@ -471,7 +471,35 @@ elif user_choice < pc_choice:
 #     print(response.text)
 
 
+# from requests import get
+#
+# base_url = "https://blog.feedback.io/?cat=120602&s="
+# search_term = "ux"
+#
+# response = get(f"{base_url}{search_term}")
+# if response.status_code != 200:
+#     print("Can't request website")
+# else:
+#     print(response.text)
+
+# 5.4 BeautifulSoup
+
+# from requests import get
+# from bs4 import BeautifulSoup
+
+# base_url = "http://weworkremotely.com/remote-jobs/serch?term="
+# search_term = "python"
+#
+# response = get(f"{base_url}{search_term}")
+# if response.status_code != 200:
+#     print("Can't request website")
+# else:
+#     soup = BeautifulSoup(response.text, "html.parser")
+#     job = soup.find_all('section', class_="jobs")
+
+
 from requests import get
+from bs4 import BeautifulSoup
 
 base_url = "https://blog.feedback.io/?cat=120602&s="
 search_term = "ux"
@@ -480,6 +508,7 @@ response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
     print("Can't request website")
 else:
-    print(response.text)
+    soup = BeautifulSoup(response.text, "html.parser")
+    title = soup.find_all("h4")
 
-# 5.4 BeautifulSoup
+# 5.5 Keyword Arguments

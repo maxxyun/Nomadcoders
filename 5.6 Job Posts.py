abe892bf3503490de242d61fefc572cc5fcca144
maxxyun <windogs09@gmail.com>
2023-12-04 18:42:48 +0900

    1204

--- 5.6 Job Posts.py
+++ 5.6 Job Posts.py
@@ -29,6 +29,7 @@ search_term = "ux"
 response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
     print("Can't request website")
+
 else:
     soup = BeautifulSoup(response.text, "html.parser")
         title = soup.find_all("div", class_"site-main")

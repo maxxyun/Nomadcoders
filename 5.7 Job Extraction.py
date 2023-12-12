b168abaaba51008e440e7abb2aa0e671d868b4eb
maxxyun <windogs09@gmail.com>
2023-12-12 18:12:54 +0900

    1212

--- 5.7 Job Extraction.py
+++ 5.7 Job Extraction.py
@@ -29,7 +29,7 @@ response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
     print("Can't request website")
 else:
-    soup = BeautifulSoup(response.text, "html.parser")
+    soup = BeautifulSoup(response.text, "html.parser")sdfsdf
     title = soup.find_all("div", class_"site-main")
     for job_section in jobs:
         print(job_section.find_all("li"))

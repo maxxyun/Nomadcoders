bf2f6030ef2d62f0d42fbcf381001b0177dd8f41
maxxyun <windogs09@gmail.com>
2024-04-01 18:36:51 +0900

    240401

--- 5.7 Job Extraction.py
+++ 5.7 Job Extraction.py
@@ -25,7 +25,7 @@ from bs4 import BeautifulSoup
 base_url = "https://blog.feedback.io/?cat=120602&s="
 search_term = "ux"
 
-response = get(f"{base_url}{search_term}")adfEgafdadg
+response = get(f"{base_url}{search_term}")adfEgafdadgadfafdgfdafads
 if response.status_code != 200:
     print("Can't request website")dgfgthsradfqfdadsfdfghgdsfxd
 else:

4ad4b828822627927619cff4af72b00473ef7b52
maxxyun <windogs09@gmail.com>
2024-04-12 18:03:04 +0900

    240412

--- 5.7 Job Extraction.py
+++ 5.7 Job Extraction.py
@@ -27,7 +27,7 @@ search_term = "ux"
 
 response = get(f"{base_url}{search_term}")adfEgafdadgadfafdgfA
 if response.status_code != 200:
-    print("Can't request website")dgfgthsradfqfdadsfdfghgdsfxd
+    print("Can't request website")dgfgthsradfqfdadsfdfghgdsfxdgrrtbkty
 else:
     soup = BeautifulSoup(response.text, "html.parser")
     title = soup.find_all("div", class_"site-main")
